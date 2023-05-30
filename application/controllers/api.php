b65d1ab22cbfd75b7a5d9c8b36797cb642081424
giressesama237 <gsiledje@gmail.com>
2023-05-30 06:50:36 +0100

    ajout de la fonction login dans le controller api

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -20,19 +20,47 @@ class Api extends CI_Controller
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
         $this->output->set_header('Pragma: no-cache');
     }
-    function getStudents($class_id){
+    function getStudents($class_id)
+    {
         $running_year = $this->db->get_where('settings', array('type' => 'running_year'))->row()->description;
-        $students =  $this->crud_model->get_students($class_id,$running_year);
+        $students =  $this->crud_model->get_students($class_id, $running_year);
         echo json_encode($students);
-            
     }
-    function getSubjectByClass($class_id){
+    function getSubjectByClass($class_id)
+    {
         $classes = $this->crud_model->get_subjects_by_class($class_id);
         echo json_encode($classes);
     }
-    function getTeacher(){
+    function getTeacher()
+    {
         $teachers = $this->crud_model->get_teachers();
         echo json_encode($teachers);
     }
-    
+    function login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $credential = array('email' => $email, 'password' => sha1($password));
+        // Checking login credential for admin
+        if(count($query = $this->db->get_where('admin', $credential)->row())){
+            $result['admin_login'] = 1;
+            $result['admin_id'] = $query->admin_id;
+            $result['login_user_id'] = $query->admin_id;
+            $result['name'] = $query->name;
+            $result['login_type'] = $query->type;
+            $result['login_error'] = 0;
+            
+
+        }else if(count($query = $this->db->get_where('teacher', $credential)->row())) {
+            $result['teacher_login'] = 1;
+            $result['teacher_id'] = $query->admin_id;
+            $result['teacher_user_id'] = $query->admin_id;
+            $result['name'] = $query->name;
+            $result['login_error'] = 0;
+            $result['login_type'] = $query->type;
+        }else{
+            $result['login_error'] = 1;
+        }
+        echo json_encode($result);
+    }
 }
